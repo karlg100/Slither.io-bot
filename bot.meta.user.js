39904396db8f8a620c4356b28d334bf96f37a0b6
karlg100 <karl@spacechimp.net>
2016-05-22 09:46:06 -0400

    Update bot.meta.user.js

--- bot.meta.user.js
+++ bot.meta.user.js
@@ -5,10 +5,10 @@
 // @description  Slither.io bot A*
 // @author       Ermiya Eskandary & Théophile Cailliau
 // @match        http://slither.io/
-// @require		   https://github.com/ErmiyaEskandary/Slither.io-bot/raw/master/btree-complete.js
-// @require		   https://github.com/ErmiyaEskandary/Slither.io-bot/raw/master/bot.astar.js
-// @require		   https://github.com/ErmiyaEskandary/Slither.io-bot/raw/master/bot.gridsystem.js
-// @require		   https://github.com/ErmiyaEskandary/Slither.io-bot/raw/master/bot.behaviors.js
-// @require		   https://github.com/ErmiyaEskandary/Slither.io-bot/raw/master/bot.user.js
+// @require		   https://github.com/Karlg100/Slither.io-bot/raw/master/btree-complete.js
+// @require		   https://github.com/Karlg100/Slither.io-bot/raw/master/bot.astar.js
+// @require		   https://github.com/Karlg100/Slither.io-bot/raw/master/bot.gridsystem.js
+// @require		   https://github.com/Karlg100/Slither.io-bot/raw/master/bot.behaviors.js
+// @require		   https://github.com/Karlg100/Slither.io-bot/raw/master/bot.user.js
 // @grant        none
 // ==/UserScript==
